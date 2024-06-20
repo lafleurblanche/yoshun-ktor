8e98644d85143344a62c48fa2e78c821b67af3f3
lafleurblanche <aquamint@squall.dti.ne.jp>
2024-06-20 22:23:54 +0900

    20240620 feat: argw router

--- src/main/kotlin/net/konohana/sakuya/yoshun/plugins/Serialization.kt
+++ src/main/kotlin/net/konohana/sakuya/yoshun/plugins/Serialization.kt
@@ -1,12 +1,13 @@
 package net.konohana.sakuya.yoshun.plugins
 
-import com.fasterxml.jackson.databind.*
-import io.ktor.serialization.jackson.*
-import io.ktor.serialization.kotlinx.json.*
-import io.ktor.server.application.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
+import com.fasterxml.jackson.databind.SerializationFeature
+import io.ktor.serialization.jackson.jackson
+import io.ktor.serialization.kotlinx.json.json
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.routing.routing
+import net.konohana.sakuya.yoshun.router.argw.argwRouter
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
@@ -16,11 +17,6 @@ fun Application.configureSerialization() {
         }
     }
     routing {
-        get("/json/kotlinx-serialization") {
-            call.respond(mapOf("hello" to "world"))
-        }
-        get("/json/jackson") {
-            call.respond(mapOf("hello" to "world"))
-        }
+        argwRouter()
     }
 }
