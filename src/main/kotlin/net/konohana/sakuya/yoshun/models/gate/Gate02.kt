3f979a27b22a357bf54f29c90ce0d866933818a7
lafleurblanche <aquamint@squall.dti.ne.jp>
2024-06-20 21:18:12 +0900

    20240620 feat: gate model

---
+++ src/main/kotlin/net/konohana/sakuya/yoshun/models/gate/Gate02.kt
@@ -0,0 +1,13 @@
+package net.konohana.sakuya.yoshun.models.gate
+
+import org.jetbrains.exposed.sql.Table
+
+
+object Gate02 : Table("gate_gt02") {
+    val id = integer("id").autoIncrement()
+    val routeID = varchar("route_id", length = 20)
+    val staCode = varchar("sta_code", length = 20)
+    val fromStaCode = varchar("from_sta_code", length = 20)
+    val toStaCode = varchar("to_sta_code", length = 20)
+    val staName = varchar("sta_name", length = 20)
+}
