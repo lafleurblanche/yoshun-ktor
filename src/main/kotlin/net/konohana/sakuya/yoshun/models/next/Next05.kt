15c45b338993171fdc46b1c2c7d28207a1d056ba
lafleurblanche <aquamint@squall.dti.ne.jp>
2024-06-20 21:12:08 +0900

    20240620 feat: next model

---
+++ src/main/kotlin/net/konohana/sakuya/yoshun/models/next/Next05.kt
@@ -0,0 +1,12 @@
+package net.konohana.sakuya.yoshun.models.next
+
+import org.jetbrains.exposed.sql.Table
+
+object Next05 : Table("next_nx05") {
+    val id = integer("id").autoIncrement()
+    val routeID = varchar("route_id", length = 20)
+    val staCode = varchar("sta_code", length = 20)
+    val fromStaCode = varchar("from_sta_code", length = 20)
+    val toStaCode = varchar("to_sta_code", length = 20)
+    val staName = varchar("sta_name", length = 20)
+}
