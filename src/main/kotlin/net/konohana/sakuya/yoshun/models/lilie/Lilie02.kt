34e133af936f0d6d2f544df91ff84034b3398cd8
lafleurblanche <aquamint@squall.dti.ne.jp>
2024-06-20 21:03:22 +0900

    20240620 feat: lilie model

---
+++ src/main/kotlin/net/konohana/sakuya/yoshun/models/lilie/Lilie02.kt
@@ -0,0 +1,13 @@
+package net.konohana.sakuya.yoshun.models.lilie
+
+import org.jetbrains.exposed.sql.Table
+
+
+object Lilie02 : Table("lilie_li02") {
+    val id = integer("id").autoIncrement()
+    val routeID = varchar("route_id", length = 20)
+    val staCode = varchar("sta_code", length = 20)
+    val fromStaCode = varchar("from_sta_code", length = 20)
+    val toStaCode = varchar("to_sta_code", length = 20)
+    val staName = varchar("sta_name", length = 20)
+}
