5e00f3efcda396b68052236bba282740cf82bf36
lafleurblanche <aquamint@squall.dti.ne.jp>
2024-06-22 12:41:09 +0900

    20240622 chore: database connect

--- src/main/kotlin/net/konohana/sakuya/yoshun/Application.kt
+++ src/main/kotlin/net/konohana/sakuya/yoshun/Application.kt
@@ -1,6 +1,7 @@
 package net.konohana.sakuya.yoshun
 
 import io.ktor.server.application.Application
+import net.konohana.sakuya.yoshun.db.KaedeDatabaseFactory
 import net.konohana.sakuya.yoshun.plugins.configureHTTP
 import net.konohana.sakuya.yoshun.plugins.configureRouting
 import net.konohana.sakuya.yoshun.plugins.configureSerialization
@@ -16,4 +17,8 @@ fun Application.module() {
     configureHTTP()
     configureRouting()
     settingKoin()
+    val kaedeUrl = environment.config.property("ktor.kaededb.uri").getString()
+    val kaedeUsername = environment.config.property("ktor.kaededb.username").getString()
+    val kaedePassword = environment.config.property("ktor.kaededb.password").getString()
+    KaedeDatabaseFactory.initKaedeDB(url = kaedeUrl, username = kaedeUsername, password = kaedePassword)
 }
