3d2a1164c4a49d5c41638bf223f850cf020a20ec
lafleurblanche <aquamint@squall.dti.ne.jp>
2024-07-01 21:37:58 +0900

    20240701 chore:

---
+++ src/main/kotlin/net/konohana/sakuya/yoshun/controller/faredist/argw/Argw01FareDistController.kt
@@ -0,0 +1,12 @@
+package net.konohana.sakuya.yoshun.controller.faredist.argw
+
+import net.konohana.sakuya.yoshun.dtos.faredist.argw.Argw01FareDistDataDto
+import net.konohana.sakuya.yoshun.services.faredist.argw.Argw01FareDistService
+
+class Argw01FareDistController (
+    private val neue01FareDistService: Argw01FareDistService
+) {
+    suspend fun getArgw01FareDistList(): List<Argw01FareDistDataDto> {
+        return neue01FareDistService.getArgw01FareDist()
+    }
+}
