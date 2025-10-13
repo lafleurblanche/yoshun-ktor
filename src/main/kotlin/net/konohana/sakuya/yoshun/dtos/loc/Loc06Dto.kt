136d1d0d77646142a81b8d9a4f714c6f81c8e59d
lafleurblanche <aquamint@squall.dti.ne.jp>
2025-10-13 22:18:34 +0900

    20251013 add

---
+++ src/main/kotlin/net/konohana/sakuya/yoshun/dtos/loc/Loc06Dto.kt
@@ -0,0 +1,13 @@
+package net.konohana.sakuya.yoshun.dtos.loc
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Loc06Dto(
+    val id: Int,
+    val routeID: String,
+    val staCode: String,
+    val fromStaCode: String,
+    val toStaCode: String,
+    val staName: String,
+)
