5fff56e1bfeeff411ec7e49fd95b106dbe0c8e36
lafleurblanche <aquamint@squall.dti.ne.jp>
2025-10-13 16:33:50 +0900

    20251013 add

---
+++ src/main/kotlin/net/konohana/sakuya/yoshun/dtos/next/Next03Dto.kt
@@ -0,0 +1,13 @@
+package net.konohana.sakuya.yoshun.dtos.next
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Next03Dto(
+    val id: Int,
+    val routeID: String,
+    val staCode: String,
+    val fromStaCode: String,
+    val toStaCode: String,
+    val staName: String,
+)
