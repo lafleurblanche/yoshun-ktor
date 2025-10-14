8cb230ae83c22700cbc1c01a88507a0b333e4705
lafleurblanche <aquamint@squall.dti.ne.jp>
2025-10-14 20:50:26 +0900

    20251014 feat: enju dto

---
+++ src/main/kotlin/net/konohana/sakuya/yoshun/dtos/enju/Enju18Dto.kt
@@ -0,0 +1,13 @@
+package net.konohana.sakuya.yoshun.dtos.enju
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Enju18Dto(
+    val id: Int,
+    val routeID: String,
+    val staCode: String,
+    val fromStaCode: String,
+    val toStaCode: String,
+    val staName: String,
+)
