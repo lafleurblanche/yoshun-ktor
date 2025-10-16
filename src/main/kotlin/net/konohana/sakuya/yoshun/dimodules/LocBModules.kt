9727fa7d32dc1b1d379adb9d03c49bf0d46a6a5f
lafleurblanche <aquamint@squall.dti.ne.jp>
2025-10-17 07:49:51 +0900

    20251016 feat: locb controller

--- src/main/kotlin/net/konohana/sakuya/yoshun/dimodules/LocBModules.kt
+++ src/main/kotlin/net/konohana/sakuya/yoshun/dimodules/LocBModules.kt
@@ -1,5 +1,9 @@
 package net.konohana.sakuya.yoshun.dimodules
 
+import net.konohana.sakuya.yoshun.controller.locb.LocB01Controller
+import net.konohana.sakuya.yoshun.controller.locb.LocB02Controller
+import net.konohana.sakuya.yoshun.controller.locb.LocB03Controller
+import net.konohana.sakuya.yoshun.controller.locb.LocB04Controller
 import net.konohana.sakuya.yoshun.services.locb.LocB01Service
 import net.konohana.sakuya.yoshun.services.locb.LocB02Service
 import net.konohana.sakuya.yoshun.services.locb.LocB03Service
@@ -15,5 +19,11 @@ object LocBModules {
         singleOf(::LocB02Service)
         singleOf(::LocB03Service)
         singleOf(::LocB04Service)
+
+        // controller
+        singleOf(::LocB01Controller)
+        singleOf(::LocB02Controller)
+        singleOf(::LocB03Controller)
+        singleOf(::LocB04Controller)
     }
 }
