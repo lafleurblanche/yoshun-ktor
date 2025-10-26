9db2c7635b1c6c52316c44361ccf490a46aba447
lafleurblanche <aquamint@squall.dti.ne.jp>
2025-10-27 00:10:47 +0900

    20251027 add

---
+++ src/main/kotlin/net/konohana/sakuya/yoshun/controller/gate/Gate01Controller.kt
@@ -0,0 +1,12 @@
+package net.konohana.sakuya.yoshun.controller.gate
+
+import net.konohana.sakuya.yoshun.dtos.gate.Gate01Dto
+import net.konohana.sakuya.yoshun.services.gate.Gate01Service
+
+class Gate01Controller(
+    private val gate01Service: Gate01Service
+) {
+    suspend fun getGate01StaList(): List<Gate01Dto> {
+        return gate01Service.getGate01()
+    }
+}
