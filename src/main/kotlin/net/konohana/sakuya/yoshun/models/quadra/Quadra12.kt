669b97980498cdef954751d526c7adf0aea47a6f
lafleurblanche <aquamint@squall.dti.ne.jp>
2025-10-31 19:06:16 +0900

    20251031 add

---
+++ src/main/kotlin/net/konohana/sakuya/yoshun/models/quadra/Quadra12.kt
@@ -0,0 +1,16 @@
+package net.konohana.sakuya.yoshun.models.quadra
+
+import org.jetbrains.exposed.sql.Table
+
+/**
+ * ## Quadra12モデル
+ * @author lafleurblanche
+ */
+object Quadra12 : Table("quadra_qd12") {
+    val id = integer("id").autoIncrement()
+    val routeID = varchar("route_id", length = 20)
+    val staCode = varchar("sta_code", length = 20)
+    val fromStaCode = varchar("from_sta_code", length = 20)
+    val toStaCode = varchar("to_sta_code", length = 20)
+    val staName = varchar("sta_name", length = 20)
+}
