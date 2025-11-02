acfddb09d6ecb40c3d41ebe9a2612ec88adb5796
lafleurblanche <aquamint@squall.dti.ne.jp>
2025-11-02 22:03:45 +0900

    20251102 refactor: url変更

--- src/main/kotlin/net/konohana/sakuya/yoshun/router/enju/EnjuRouter.kt
+++ src/main/kotlin/net/konohana/sakuya/yoshun/router/enju/EnjuRouter.kt
@@ -14,19 +14,21 @@ fun Route.enjuRouter() {
     val enju01Controller by inject<Enju01Controller>()
     val enju02Controller by inject<Enju02Controller>()
     val enju03Controller by inject<Enju03Controller>()
-    route("enju01") {
-        get {
-            call.respond(enju01Controller.getEnju01StaList())
+    route("cerisier") {
+        route("enju01") {
+            get {
+                call.respond(enju01Controller.getEnju01StaList())
+            }
         }
-    }
-    route("enju02") {
-        get {
-            call.respond(enju02Controller.getEnju02StaList())
+        route("enju02") {
+            get {
+                call.respond(enju02Controller.getEnju02StaList())
+            }
         }
-    }
-    route("enju03") {
-        get {
-            call.respond(enju03Controller.getEnju03StaList())
+        route("enju03") {
+            get {
+                call.respond(enju03Controller.getEnju03StaList())
+            }
         }
     }
 }
