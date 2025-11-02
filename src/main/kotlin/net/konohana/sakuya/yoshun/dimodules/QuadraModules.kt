bae6edb62ba847cf97cdb890b72b196f49ad4f5d
lafleurblanche <aquamint@squall.dti.ne.jp>
2025-11-02 18:11:20 +0900

    20251102 add

--- src/main/kotlin/net/konohana/sakuya/yoshun/dimodules/QuadraModules.kt
+++ src/main/kotlin/net/konohana/sakuya/yoshun/dimodules/QuadraModules.kt
@@ -1,5 +1,14 @@
 package net.konohana.sakuya.yoshun.dimodules
 
+import net.konohana.sakuya.yoshun.controller.quadra.Quadra01Controller
+import net.konohana.sakuya.yoshun.controller.quadra.Quadra02Controller
+import net.konohana.sakuya.yoshun.controller.quadra.Quadra03Controller
+import net.konohana.sakuya.yoshun.controller.quadra.Quadra04Controller
+import net.konohana.sakuya.yoshun.controller.quadra.Quadra05Controller
+import net.konohana.sakuya.yoshun.controller.quadra.Quadra06Controller
+import net.konohana.sakuya.yoshun.controller.quadra.Quadra07Controller
+import net.konohana.sakuya.yoshun.controller.quadra.Quadra08Controller
+import net.konohana.sakuya.yoshun.controller.quadra.Quadra09Controller
 import net.konohana.sakuya.yoshun.services.quadra.Quadra01Service
 import net.konohana.sakuya.yoshun.services.quadra.Quadra02Service
 import net.konohana.sakuya.yoshun.services.quadra.Quadra03Service
@@ -33,6 +42,15 @@ object QuadraModules {
         singleOf(::Quadra12Service)
 
         // controllers
+        singleOf(::Quadra01Controller)
+        singleOf(::Quadra02Controller)
+        singleOf(::Quadra03Controller)
+        singleOf(::Quadra04Controller)
+        singleOf(::Quadra05Controller)
+        singleOf(::Quadra06Controller)
+        singleOf(::Quadra07Controller)
+        singleOf(::Quadra08Controller)
+        singleOf(::Quadra09Controller)
 
     }
 }
