8456b79dea80f233c37ebbae93209a7684db79bc
lafleurblanche <aquamint@squall.dti.ne.jp>
2025-11-05 18:31:08 +0900

    20251105 add

---
+++ src/main/kotlin/net/konohana/sakuya/yoshun/controller/enju/Enju05Controller.kt
@@ -0,0 +1,12 @@
+package net.konohana.sakuya.yoshun.controller.enju
+
+import net.konohana.sakuya.yoshun.dtos.enju.Enju05Dto
+import net.konohana.sakuya.yoshun.services.enju.Enju05Service
+
+class Enju05Controller(
+    private val enju05Service: Enju05Service
+) {
+    suspend fun getEnju05StaList(): List<Enju05Dto> {
+        return enju05Service.getEnju05()
+    }
+}
