111482899e016ace51d5ff38bfee558ea97c5037
lafleurblanche <aquamint@squall.dti.ne.jp>
2025-11-11 01:34:41 +0900

    20251111 Add

--- src/main/kotlin/net/konohana/sakuya/yoshun/controller/quadra/Quadra04Controller.kt
+++ src/main/kotlin/net/konohana/sakuya/yoshun/controller/quadra/Quadra04Controller.kt
@@ -1,5 +1,6 @@
 package net.konohana.sakuya.yoshun.controller.quadra
 
+import net.konohana.sakuya.yoshun.dtos.quadra.Quadra04FrontendDto
 import net.konohana.sakuya.yoshun.dtos.quadra.Quadra04Dto
 import net.konohana.sakuya.yoshun.services.quadra.Quadra04Service
 
@@ -12,5 +13,10 @@ class Quadra04Controller(
 
     suspend fun getQuadra04StaListByStaCode(staCode: String): Quadra04Dto? {
         return quadra04Service.getQuadra04ByStaCode(staCode = staCode)
+    }
+
+    suspend fun getQuadra04FrontendList(): List<Quadra04FrontendDto> {
+        // サービス層の新しいメソッドを呼び出す
+        return quadra04Service.getQuadra04Frontend()
     }
 }
