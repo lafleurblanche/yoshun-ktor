309ba84296a9ea5750ca0f4f73c0b195d9503799
lafleurblanche <aquamint@squall.dti.ne.jp>
2025-11-12 20:44:00 +0900

    20251112 Add

---
+++ src/main/kotlin/net/konohana/sakuya/yoshun/dtos/quadra/Quadra19FrontendDto.kt
@@ -0,0 +1,12 @@
+package net.konohana.sakuya.yoshun.dtos.quadra
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Quadra19FrontendDto(
+    val id: Int,
+    val viaRouteName: String,
+    val staCode: String,
+    val staName1: String,
+    val staName2: String,
+)
