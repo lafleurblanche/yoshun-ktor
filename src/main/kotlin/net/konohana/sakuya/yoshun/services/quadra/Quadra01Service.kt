b82a1a1d80ad8473efdee7ea9cd011a5fb66f5ea
lafleurblanche <aquamint@squall.dti.ne.jp>
2025-11-12 20:37:50 +0900

    20251112 Fix #79

--- src/main/kotlin/net/konohana/sakuya/yoshun/services/quadra/Quadra01Service.kt
+++ src/main/kotlin/net/konohana/sakuya/yoshun/services/quadra/Quadra01Service.kt
@@ -39,7 +39,6 @@ class Quadra01Service {
             staCode = row[Quadra01.staCode],
             staName1 = staName1,
             staName2 = staName2,
-            staName = staName,
         )
     }
 
