feb25effa0e4581db9bf9ad1523fd8c88d0303bf
lafleurblanche <aquamint@squall.dti.ne.jp>
2025-11-12 20:59:08 +0900

    20251112 Fix public

--- src/main/kotlin/net/konohana/sakuya/yoshun/constants/CommonConst.kt
+++ src/main/kotlin/net/konohana/sakuya/yoshun/constants/CommonConst.kt
@@ -3,9 +3,9 @@ package net.konohana.sakuya.yoshun.constants
 class CommonConst {
     companion object {
         // staNameを分割する際の長さ（staName1の最大長、substringのステップ）
-        private const val SPLIT_LENGTH = 2
+        const val SPLIT_LENGTH = 2
 
         // 分割処理を行うStaNameの最小長さ（staName1とstaName2に分ける条件）
-        private const val MIN_LENGTH_FOR_SPLIT = 4
+        const val MIN_LENGTH_FOR_SPLIT = 4
     }
 }
