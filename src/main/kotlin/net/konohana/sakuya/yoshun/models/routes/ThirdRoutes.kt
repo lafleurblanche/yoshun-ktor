2b89e1363a7fb475961dedbab03f132d77a18e33
lafleurblanche <aquamint@squall.dti.ne.jp>
2025-11-14 00:59:26 +0900

    20251114 Add

---
+++ src/main/kotlin/net/konohana/sakuya/yoshun/models/routes/ThirdRoutes.kt
@@ -0,0 +1,8 @@
+package net.konohana.sakuya.yoshun.models.routes
+
+import org.jetbrains.exposed.sql.Table
+
+object ThirdRoutes : Table("third_routes") {
+    val routeID = varchar("route_id", 10).uniqueIndex()
+    val viaRouteName = varchar("via_route_name", 100)
+}
