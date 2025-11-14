d4c2bcfabda3f649074ea606eb8c490ed319f81e
lafleurblanche <aquamint@squall.dti.ne.jp>
2025-11-14 22:18:16 +0900

    20251114 Add

---
+++ src/main/kotlin/net/konohana/sakuya/yoshun/dtos/third/Third02FrontendDto.kt
@@ -0,0 +1,12 @@
+package net.konohana.sakuya.yoshun.dtos.third
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Third02FrontendDto(
+    val id: Int,
+    val viaRouteName: String,
+    val staCode: String,
+    val staName1: String,
+    val staName2: String,
+)
