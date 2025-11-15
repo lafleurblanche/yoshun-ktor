1b30bb4a553c47eca0d6e4da48229fc6741b47be
lafleurblanche <aquamint@squall.dti.ne.jp>
2025-11-15 09:09:41 +0900

    20251115 Add

--- src/main/kotlin/net/konohana/sakuya/yoshun/controller/third/Third02Controller.kt
+++ src/main/kotlin/net/konohana/sakuya/yoshun/controller/third/Third02Controller.kt
@@ -1,5 +1,6 @@
 package net.konohana.sakuya.yoshun.controller.third
 
+import net.konohana.sakuya.yoshun.dtos.third.Third02FrontendDto
 import net.konohana.sakuya.yoshun.dtos.third.Third02Dto
 import net.konohana.sakuya.yoshun.services.third.Third02Service
 
@@ -12,5 +13,10 @@ class Third02Controller(
 
     suspend fun getThird02StaListByStaCode(staCode: String): Third02Dto? {
         return third02Service.getThird02ByStaCode(staCode = staCode)
+    }
+
+    suspend fun getThird02FrontendList(): List<Third02FrontendDto> {
+        // サービス層の新しいメソッドを呼び出す
+        return third02Service.getThird02Frontend()
     }
 }
