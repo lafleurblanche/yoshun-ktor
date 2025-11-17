293e740b378765373b86d792a35d7ab06cc19e60
lafleurblanche <aquamint@squall.dti.ne.jp>
2025-11-17 20:36:16 +0900

    20251117 Add

--- src/main/kotlin/net/konohana/sakuya/yoshun/services/next/Next03Service.kt
+++ src/main/kotlin/net/konohana/sakuya/yoshun/services/next/Next03Service.kt
@@ -1,9 +1,14 @@
 package net.konohana.sakuya.yoshun.services.next
 
+import net.konohana.sakuya.yoshun.constants.CommonConst.Companion.MIN_LENGTH_FOR_SPLIT
+import net.konohana.sakuya.yoshun.constants.CommonConst.Companion.SPLIT_LENGTH
 import net.konohana.sakuya.yoshun.db.KaedeDatabaseFactory
 import net.konohana.sakuya.yoshun.dtos.next.Next03Dto
+import net.konohana.sakuya.yoshun.dtos.next.Next03FrontendDto
 import net.konohana.sakuya.yoshun.models.next.Next03
+import net.konohana.sakuya.yoshun.models.routes.NextRoutes
 import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.leftJoin
 import org.jetbrains.exposed.sql.selectAll
 
 class Next03Service {
@@ -15,6 +20,41 @@ class Next03Service {
         staCode = row[Next03.staCode],
         staName = row[Next03.staName],
     )
+
+    private fun resultRowNext03Frontend(row: ResultRow): Next03FrontendDto {
+
+        val staName = row[Next03.staName]
+        val (staName1, staName2) = when {
+            staName.length >= MIN_LENGTH_FOR_SPLIT -> Pair(
+                staName.take(SPLIT_LENGTH),
+                staName.drop(SPLIT_LENGTH).take(SPLIT_LENGTH)
+            )
+            else -> Pair(staName, "")
+        }
+        val viaRouteName = row.getOrNull(NextRoutes.viaRouteName) ?: ""
+
+        return Next03FrontendDto(
+            id = row[Next03.id],
+            viaRouteName = viaRouteName,
+            staCode = row[Next03.staCode],
+            staName1 = staName1,
+            staName2 = staName2,
+        )
+    }
+
+    suspend fun getNext03Frontend(): List<Next03FrontendDto> {
+        return KaedeDatabaseFactory.dbQuery {
+            Next03
+
+                .leftJoin(
+                    NextRoutes,
+                    { Next03.routeID },
+                    { NextRoutes.routeID }
+                )
+                .selectAll()
+                .map(::resultRowNext03Frontend)
+        }
+    }
 
     suspend fun getNext03(): List<Next03Dto> {
         return KaedeDatabaseFactory.dbQuery {
