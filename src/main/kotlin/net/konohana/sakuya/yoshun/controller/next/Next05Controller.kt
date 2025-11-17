6036a40a984337bc0e0802ee78f90109ca30fb3a
lafleurblanche <aquamint@squall.dti.ne.jp>
2025-11-17 20:35:42 +0900

    20251117 Add

--- src/main/kotlin/net/konohana/sakuya/yoshun/controller/next/Next05Controller.kt
+++ src/main/kotlin/net/konohana/sakuya/yoshun/controller/next/Next05Controller.kt
@@ -1,5 +1,6 @@
 package net.konohana.sakuya.yoshun.controller.next
 
+import net.konohana.sakuya.yoshun.dtos.next.Next05FrontendDto
 import net.konohana.sakuya.yoshun.dtos.next.Next05Dto
 import net.konohana.sakuya.yoshun.services.next.Next05Service
 
@@ -12,5 +13,10 @@ class Next05Controller(
 
     suspend fun getNext05StaListByStaCode(staCode: String): Next05Dto? {
         return next05Service.getNext05ByStaCode(staCode = staCode)
+    }
+
+    suspend fun getNext05FrontendList(): List<Next05FrontendDto> {
+        // サービス層の新しいメソッドを呼び出す
+        return next05Service.getNext05Frontend()
     }
 }
